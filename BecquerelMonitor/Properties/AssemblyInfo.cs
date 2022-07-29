bcc6be2f1fac622e723a6f664d0273833548c8a6
Amber <morozov.albert@yandex.ru>
2022-07-29 20:22:41 +0300

    Fix bugs

--- BecquerelMonitor/Properties/AssemblyInfo.cs
+++ BecquerelMonitor/Properties/AssemblyInfo.cs
@@ -4,13 +4,13 @@ using System.Resources;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("2022.07.28.3")]
+[assembly: AssemblyVersion("2022.07.28.4")]
 [assembly: AssemblyProduct("BecquerelMonitor")]
 [assembly: AssemblyCopyright("free")]
 [assembly: AssemblyTrademark("none")]
 [assembly: AssemblyConfiguration("")]
 [assembly: Guid("40110b38-4882-47c1-ad94-a71e58dcb5f8")]
-[assembly: AssemblyFileVersion("2022.07.28.3")]
+[assembly: AssemblyFileVersion("2022.07.28.4")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyCompany("free")]
 [assembly: CompilationRelaxations(8)]
