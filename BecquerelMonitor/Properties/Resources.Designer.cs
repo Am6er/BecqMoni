b166739e3cdbdad72a065619400897152f6e72fa
Amber <morozov.albert@yandex.ru>
2022-08-09 15:31:13 +0300

    Add Attach icon

--- BecquerelMonitor/Properties/Resources.Designer.cs
+++ BecquerelMonitor/Properties/Resources.Designer.cs
@@ -88,6 +88,16 @@ namespace BecquerelMonitor.Properties {
         }
         
         /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap attach {
+            get {
+                object obj = ResourceManager.GetObject("attach", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to Attach.
         /// </summary>
         internal static string Attach_BTN {
