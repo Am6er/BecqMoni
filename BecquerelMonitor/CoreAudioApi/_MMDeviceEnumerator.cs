e710f33d0dbd5a30bc1cf1fe493840ca11e20743
Amber <morozov.albert@yandex.ru>
2023-09-12 12:24:47 +0300

    Fix. Autosetting audio spectrometer checkbox now sets current device Volume.

--- BecquerelMonitor/CoreAudioApi/_MMDeviceEnumerator.cs
+++ BecquerelMonitor/CoreAudioApi/_MMDeviceEnumerator.cs
@@ -6,11 +6,11 @@ namespace CoreAudioApi
 {
     // Token: 0x020001D6 RID: 470
     [Guid("BCDE0395-E52F-467C-8E3D-C4579291692E")]
-    //[ComImport]
+    [ComImport]
     class _MMDeviceEnumerator
     {
         // Token: 0x0600164C RID: 5708
-        [MethodImpl(MethodImplOptions.InternalCall)]
-        public extern _MMDeviceEnumerator();
+        //[MethodImpl(MethodImplOptions.InternalCall)]
+        //public extern _MMDeviceEnumerator();
     }
 }
