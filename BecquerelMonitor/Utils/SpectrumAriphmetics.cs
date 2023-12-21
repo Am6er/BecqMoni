03869ad20c36862a1e253547d3029746894e8c5e
Amber <morozov.albert@yandex.ru>
2023-12-21 22:41:51 +0300

    Fix overflow error for long-time spectras.

--- BecquerelMonitor/Utils/SpectrumAriphmetics.cs
+++ BecquerelMonitor/Utils/SpectrumAriphmetics.cs
@@ -4,7 +4,6 @@ using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
-using System.Runtime.Serialization.Formatters;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
