bb0507ade2c4129be46f7fdf8f7cffb846064d8a
Am6er <morozov.albert@yandex.ru>
2024-02-21 13:37:36 +0300

    Increase calibration energy limit up to 100MeV. Add Radiacode ROI preset.

--- BecquerelMonitor/PolynomialEnergyCalibration.cs
+++ BecquerelMonitor/PolynomialEnergyCalibration.cs
@@ -123,7 +123,7 @@ namespace BecquerelMonitor
             for (int i = 1; i <= channels; i++)
             {
                 double prevEnrg = this.ChannelToEnergy(i - 1);
-                if (prevEnrg <= -1000.0 || prevEnrg >= 10000.0) return false;
+                if (prevEnrg <= -1000.0 || prevEnrg >= 100000.0) return false;
                 if (prevEnrg > this.ChannelToEnergy(i))
                 {
                     return false;
