a9ac5c29b21d79d8d8332ae23d6e964c7e1c2833
Am6er <morozov.albert@yandex.ru>
2024-04-17 13:18:47 +0300

    Some tune X scale

--- BecquerelMonitor/EnergySpectrumView.Designer.cs
+++ BecquerelMonitor/EnergySpectrumView.Designer.cs
@@ -3736,7 +3736,7 @@ namespace BecquerelMonitor
         public void FitHorizontalScale()
         {
             int num = base.Width - this.left - this.vScrollBar1.Width - 4;
-            this.horizontalScale = (double)num / (this.energyCalibration.ChannelToEnergy((double)this.numberOfChannels) * this.pixelPerEnergy + 18.0);
+            this.horizontalScale = (double)num / (this.energyCalibration.ChannelToEnergy((double)this.numberOfChannels) * this.pixelPerEnergy + 20.0);
             if (ActionEvent != null) ActionEvent(this, new EnergySpectrumActionEventArgs(true, (decimal)this.horizontalScale));
             int maximum = this.CalcMaximumXValue() + 5;
             this.hScrollBar1.Maximum = maximum;
@@ -3776,7 +3776,7 @@ namespace BecquerelMonitor
             }
             else
             {
-                this.horizontalScale = (double)num / (this.energyCalibration.ChannelToEnergy((double)this.numberOfChannels) * this.pixelPerEnergy + 18.0);
+                this.horizontalScale = (double)num / (this.energyCalibration.ChannelToEnergy((double)this.numberOfChannels) * this.pixelPerEnergy + 20.0);
                 int maximum = this.CalcMaximumXValue() + 5;
                 this.hScrollBar1.Maximum = maximum;
                 this.hScrollBar1.Value = 0;
