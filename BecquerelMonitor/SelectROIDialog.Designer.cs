bab732c3d773363f76235f7f08dbab7c260fa5bf
Am6er <morozov.albert@yandex.ru>
2024-05-20 02:32:51 +0300

    Add normalization spectrum according roi w/o default roi efficiencny in device settings

---
+++ BecquerelMonitor/SelectROIDialog.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace BecquerelMonitor
+{
+    partial class SelectROIDialog
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SelectROIDialog));
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            resources.ApplyResources(this.comboBox1, "comboBox1");
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // button1
+            // 
+            resources.ApplyResources(this.button1, "button1");
+            this.button1.Name = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            resources.ApplyResources(this.button2, "button2");
+            this.button2.Name = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // SelectROIDialog
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "SelectROIDialog";
+            this.ShowInTaskbar = false;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
---
+++ BecquerelMonitor/SelectROIDialog.cs
@@ -0,0 +1,96 @@
+﻿using BecquerelMonitor.Properties;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BecquerelMonitor
+{
+    public partial class SelectROIDialog : Form
+    {
+        MainForm mainForm;
+        string returnGUID = null;
+        readonly ROIConfigManager ROIConfigManager;
+        readonly List<ROIConfigData> ROIConfigDatas;
+
+
+        public SelectROIDialog()
+        {
+            InitializeComponent();
+        }
+
+        public SelectROIDialog(Form mainForm)
+        {
+            InitializeComponent();
+            this.mainForm = (MainForm)mainForm;
+            this.Icon = Resources.becqmoni;
+            this.ROIConfigManager = ROIConfigManager.GetInstance();
+            this.ROIConfigDatas = this.ROIConfigManager.ROIConfigList;
+            base.SuspendLayout();
+            FillROIConfigs();
+            base.ResumeLayout();
+        }
+
+        private void FillROIConfigs()
+        {
+            if (ROIConfigDatas == null || ROIConfigDatas.Count == 0) return;
+
+            Parallel.For(0, ROIConfigDatas.Count, i => 
+            {
+                int counterData = 0;
+                for (int j = 0; j < ROIConfigDatas[i].ROIDefinitions.Count; j++)
+                    {
+                        if (ROIConfigDatas[i].ROIDefinitions[j].PeakEnergy > 0 && ROIConfigDatas[i].ROIDefinitions[j].Intencity > 0)
+                        {
+                            counterData++;
+                            if (counterData > 2)
+                            {
+                                comboBox1.Items.Add(ROIConfigDatas[i].Name);
+                                if (comboBox1.SelectedIndex < 0) comboBox1.SelectedIndex = 0;
+                                break;
+                            }
+                        }
+                    }
+            });
+        }
+
+        public string SendData()
+        {
+            return this.returnGUID;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.Items.Count == 0 || comboBox1.SelectedIndex < 0)
+            {
+                Close();
+            }
+
+            for (int i = 0; i < ROIConfigDatas.Count; i++)
+            {
+                if (ROIConfigDatas[i].Name == comboBox1.SelectedItem.ToString())
+                {
+                    this.returnGUID = ROIConfigDatas[i].Guid;
+                    break;
+                }
+            }
+
+            Close();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
