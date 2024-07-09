e1e8ebc09faa37b9a474682b82aa5853b2b3f8ef
Amber <morozov.albert@yandex.ru>
2024-07-09 13:49:09 +0300

    Refactoring for Designer-friendly behaviour. AboutForm.

--- BecquerelMonitor/AboutForm.Designer.cs
+++ BecquerelMonitor/AboutForm.Designer.cs
@@ -1,10 +1,14 @@
-﻿namespace BecquerelMonitor
+﻿using System.Windows.Forms;
+
+namespace BecquerelMonitor
 {
-	// Token: 0x020000C5 RID: 197
-	public partial class AboutForm : global::System.Windows.Forms.Form
+	public partial class AboutForm : Form
 	{
-		// Token: 0x06000976 RID: 2422 RVA: 0x00037750 File Offset: 0x00035950
-		protected override void Dispose(bool disposing)
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
 		{
 			if (disposing && this.components != null)
 			{
@@ -13,88 +17,107 @@
 			base.Dispose(disposing);
 		}
 
-		// Token: 0x06000977 RID: 2423 RVA: 0x00037778 File Offset: 0x00035978
-		void InitializeComponent()
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        void InitializeComponent()
 		{
-			
-			this.label1 = new global::System.Windows.Forms.Label();
-			this.label2 = new global::System.Windows.Forms.Label();
-			this.label3 = new global::System.Windows.Forms.Label();
-			this.label4 = new global::System.Windows.Forms.Label();
-			this.label5 = new global::System.Windows.Forms.Label();
-			this.button1 = new global::System.Windows.Forms.Button();
-			this.textBox1 = new global::System.Windows.Forms.TextBox();
-			this.iconPanel1 = new global::BecquerelMonitor.IconPanel();
-			this.label6 = new global::System.Windows.Forms.Label();
-			base.SuspendLayout();
-			
-			this.label1.Name = "label1";
-			
-			this.label2.Name = "label2";
-			
-			this.label3.Name = "label3";
-			
-			this.label4.Name = "label4";
-			
-			this.label5.Name = "label5";
-			
-			this.button1.Name = "button1";
-			this.button1.UseVisualStyleBackColor = true;
-			this.button1.Click += new global::System.EventHandler(this.button1_Click);
-			
-			this.textBox1.Name = "textBox1";
-			this.textBox1.ReadOnly = true;
-			this.textBox1.TextChanged += new global::System.EventHandler(this.textBox1_TextChanged);
-			
-			this.iconPanel1.Name = "iconPanel1";
-			
-			this.label6.Name = "label6";
-			
-			base.AutoScaleMode = global::System.Windows.Forms.AutoScaleMode.Font;
-			this.BackColor = global::System.Drawing.Color.AliceBlue;
-			base.Controls.Add(this.label6);
-			base.Controls.Add(this.iconPanel1);
-			base.Controls.Add(this.textBox1);
-			base.Controls.Add(this.button1);
-			base.Controls.Add(this.label5);
-			base.Controls.Add(this.label4);
-			base.Controls.Add(this.label3);
-			base.Controls.Add(this.label2);
-			base.Controls.Add(this.label1);
-			base.Name = "AboutForm";
-			base.SizeChanged += new global::System.EventHandler(this.AboutForm_SizeChanged);
-			base.ResumeLayout(false);
-			base.PerformLayout();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AboutForm));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.iconPanel1 = new BecquerelMonitor.IconPanel();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Name = "label2";
+            // 
+            // label3
+            // 
+            resources.ApplyResources(this.label3, "label3");
+            this.label3.Name = "label3";
+            // 
+            // label4
+            // 
+            resources.ApplyResources(this.label4, "label4");
+            this.label4.Name = "label4";
+            // 
+            // label5
+            // 
+            resources.ApplyResources(this.label5, "label5");
+            this.label5.Name = "label5";
+            // 
+            // button1
+            // 
+            resources.ApplyResources(this.button1, "button1");
+            this.button1.Name = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            resources.ApplyResources(this.textBox1, "textBox1");
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            // 
+            // iconPanel1
+            // 
+            resources.ApplyResources(this.iconPanel1, "iconPanel1");
+            this.iconPanel1.Name = "iconPanel1";
+            // 
+            // label6
+            // 
+            resources.ApplyResources(this.label6, "label6");
+            this.label6.Name = "label6";
+            // 
+            // AboutForm
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.AliceBlue;
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.iconPanel1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "AboutForm";
+            this.ShowInTaskbar = false;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
 		}
 
-		// Token: 0x04000545 RID: 1349
-		global::System.ComponentModel.IContainer components;
+        #endregion
 
-		// Token: 0x04000546 RID: 1350
-		global::System.Windows.Forms.Label label1;
-
-		// Token: 0x04000547 RID: 1351
-		global::System.Windows.Forms.Label label2;
-
-		// Token: 0x04000548 RID: 1352
-		global::System.Windows.Forms.Label label3;
-
-		// Token: 0x04000549 RID: 1353
-		global::System.Windows.Forms.Label label4;
-
-		// Token: 0x0400054A RID: 1354
-		global::System.Windows.Forms.Label label5;
-
-		// Token: 0x0400054B RID: 1355
-		global::System.Windows.Forms.Button button1;
-
-		// Token: 0x0400054C RID: 1356
-		global::System.Windows.Forms.TextBox textBox1;
-
-		// Token: 0x0400054D RID: 1357
-		global::BecquerelMonitor.IconPanel iconPanel1;
-
-		// Token: 0x0400054E RID: 1358
-		global::System.Windows.Forms.Label label6;
+        System.ComponentModel.IContainer components;
+		System.Windows.Forms.Label label1;
+		System.Windows.Forms.Label label2;
+		System.Windows.Forms.Label label3;
+		System.Windows.Forms.Label label4;
+		System.Windows.Forms.Label label5;
+		System.Windows.Forms.Button button1;
+		System.Windows.Forms.TextBox textBox1;
+		BecquerelMonitor.IconPanel iconPanel1;
+		System.Windows.Forms.Label label6;
 	}
 }
--- BecquerelMonitor/AboutForm.cs
+++ BecquerelMonitor/AboutForm.cs
@@ -4,58 +4,19 @@ using System.Windows.Forms;
 
 namespace BecquerelMonitor
 {
-    // Token: 0x020000C5 RID: 197
     public partial class AboutForm : Form
     {
-        // Token: 0x06000978 RID: 2424 RVA: 0x00037A54 File Offset: 0x00035C54
         public AboutForm()
         {
             this.InitializeComponent();
-
-            global::System.ComponentModel.ComponentResourceManager componentResourceManager = new global::System.ComponentModel.ComponentResourceManager(typeof(global::BecquerelMonitor.AboutForm));
-            componentResourceManager.ApplyResources(this.label1, "label1");
-            componentResourceManager.ApplyResources(this.label2, "label2");
-            componentResourceManager.ApplyResources(this.label3, "label3");
-            componentResourceManager.ApplyResources(this.label4, "label4");
-            componentResourceManager.ApplyResources(this.label5, "label5");
-            componentResourceManager.ApplyResources(this.button1, "button1");
-            componentResourceManager.ApplyResources(this.textBox1, "textBox1");
-            componentResourceManager.ApplyResources(this.iconPanel1, "iconPanel1");
-            componentResourceManager.ApplyResources(this.label6, "label6");
-            componentResourceManager.ApplyResources(this, "$this");
-
-
             base.Icon = Resources.becqmoni;
-
             this.label2.Text = "Version " + GlobalConfigManager.GetInstance().VersionString;
             this.textBox1.Text = Resources.LibraryLicensesMessage;
-            this.RecalcPosition();
         }
 
-        // Token: 0x06000979 RID: 2425 RVA: 0x00037AB4 File Offset: 0x00035CB4
         void button1_Click(object sender, EventArgs e)
         {
             base.Close();
-        }
-
-        // Token: 0x0600097A RID: 2426 RVA: 0x00037ABC File Offset: 0x00035CBC
-        void textBox1_TextChanged(object sender, EventArgs e)
-        {
-        }
-
-        // Token: 0x0600097B RID: 2427 RVA: 0x00037AC0 File Offset: 0x00035CC0
-        void AboutForm_SizeChanged(object sender, EventArgs e)
-        {
-            this.RecalcPosition();
-        }
-
-        // Token: 0x0600097C RID: 2428 RVA: 0x00037AC8 File Offset: 0x00035CC8
-        void RecalcPosition()
-        {
-            this.iconPanel1.Width = this.iconPanel1.Height;
-            this.label1.Left = this.iconPanel1.Width + 20;
-            this.label2.Left = this.iconPanel1.Width + 20;
-            this.label3.Left = this.iconPanel1.Width + 20;
         }
     }
 }
