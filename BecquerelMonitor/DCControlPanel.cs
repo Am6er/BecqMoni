c0dab56daf0fec925de5561c240c1705ce8a3ccb
Am6er <morozov.albert@yandex.ru>
2024-09-06 17:56:56 +0300

    Fix. Increase mantis for DeadTime% f2 > f4

--- BecquerelMonitor/DCControlPanel.cs
+++ BecquerelMonitor/DCControlPanel.cs
@@ -368,7 +368,7 @@ namespace BecquerelMonitor
                 deadTime = (double)activeResultData.EnergySpectrum.LiveTime / totalSeconds;
             }
             this.countRateTextBox.Text = cps.ToString("f2");
-            this.deadTimetextBox.Text = deadTime.ToString("f2");
+            this.deadTimetextBox.Text = deadTime.ToString("f4");
         }
 
         // Token: 0x06000297 RID: 663 RVA: 0x0000BAD8 File Offset: 0x00009CD8
