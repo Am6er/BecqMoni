bcce29755b7dd29814762c9f44350224b0bf5066
Amber <morozov.albert@yandex.ru>
2024-10-07 18:34:59 +0300

    Fix concurent access to DetectedPeaks list

--- BecquerelMonitor/EnergySpectrumView.Designer.cs
+++ BecquerelMonitor/EnergySpectrumView.Designer.cs
@@ -682,13 +682,10 @@ namespace BecquerelMonitor
                 this.continuumEnergySpectrum = sa.Continuum();
 
                 this.peakEnergySpectrum.Clear();
-                if (this.activeResultData.DetectedPeaks.Count > 0)
+                foreach(Peak peak in new List<Peak>(this.activeResultData.DetectedPeaks))
                 {
-                    for (int i = 0; i < this.activeResultData.DetectedPeaks.Count; i++)
-                    {
-                        (int[] peakSpectrum, int min_ch, int max_ch, Color peakColor) = sa.GetPeak(this.activeResultData.DetectedPeaks[i], this.continuumEnergySpectrum, true);
-                        this.peakEnergySpectrum.Add((peakSpectrum, min_ch, max_ch, peakColor));
-                    }
+                    (int[] peakSpectrum, int min_ch, int max_ch, Color peakColor) = sa.GetPeak(peak, this.continuumEnergySpectrum, true);
+                    this.peakEnergySpectrum.Add((peakSpectrum, min_ch, max_ch, peakColor));
                 }
 
                 sa.Dispose();
@@ -2928,7 +2925,7 @@ namespace BecquerelMonitor
             Peak peak = null;
             Peak peak2 = null;
             int num = 0;
-            foreach (Peak peak3 in this.activeResultData.DetectedPeaks)
+            foreach (Peak peak3 in new List<Peak>(this.activeResultData.DetectedPeaks))
             {
                 int channel = peak3.Channel;
                 int num2;
@@ -2952,7 +2949,7 @@ namespace BecquerelMonitor
             {
                 peak = peak2;
             }
-            foreach (Peak peak4 in this.activeResultData.DetectedPeaks)
+            foreach (Peak peak4 in new List<Peak>(this.activeResultData.DetectedPeaks))
             {
                 int channel2 = peak4.Channel;
                 int num4;
@@ -3449,7 +3446,7 @@ namespace BecquerelMonitor
                             {
                                 int number_of_peaks = 0;
                                 Peak detected_peak = null;
-                                foreach (Peak peak in this.activeResultData.DetectedPeaks)
+                                foreach (Peak peak in new List<Peak>(this.activeResultData.DetectedPeaks))
                                 {
                                     if (start_energy < peak.Energy && end_energy > peak.Energy)
                                     {
