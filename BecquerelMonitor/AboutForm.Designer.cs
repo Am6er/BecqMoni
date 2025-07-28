dc4be13773592a8bcfb2631dbdc245c2b84719bc
Amber <morozov.albert@yandex.ru>
2025-07-28 15:08:01 +0300

    AboutForm. Add clicks to open URL/s in browser.

--- BecquerelMonitor/AboutForm.Designer.cs
+++ BecquerelMonitor/AboutForm.Designer.cs
@@ -33,8 +33,11 @@ namespace BecquerelMonitor
             this.label5 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
-            this.iconPanel1 = new BecquerelMonitor.IconPanel();
             this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.iconPanel1 = new BecquerelMonitor.IconPanel();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
@@ -75,21 +78,48 @@ namespace BecquerelMonitor
             this.textBox1.Name = "textBox1";
             this.textBox1.ReadOnly = true;
             // 
+            // label6
+            // 
+            resources.ApplyResources(this.label6, "label6");
+            this.label6.Name = "label6";
+            // 
+            // label7
+            // 
+            resources.ApplyResources(this.label7, "label7");
+            this.label7.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.label7.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.label7.Name = "label7";
+            this.label7.Click += new System.EventHandler(this.label7_Click);
+            // 
             // iconPanel1
             // 
             resources.ApplyResources(this.iconPanel1, "iconPanel1");
             this.iconPanel1.Name = "iconPanel1";
             // 
-            // label6
+            // label8
             // 
-            resources.ApplyResources(this.label6, "label6");
-            this.label6.Name = "label6";
+            resources.ApplyResources(this.label8, "label8");
+            this.label8.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.label8.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.label8.Name = "label8";
+            this.label8.Click += new System.EventHandler(this.label8_Click);
+            // 
+            // label9
+            // 
+            resources.ApplyResources(this.label9, "label9");
+            this.label9.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.label9.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.label9.Name = "label9";
+            this.label9.Click += new System.EventHandler(this.label9_Click);
             // 
             // AboutForm
             // 
             resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.AliceBlue;
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.iconPanel1);
             this.Controls.Add(this.textBox1);
@@ -119,5 +149,8 @@ namespace BecquerelMonitor
 		System.Windows.Forms.TextBox textBox1;
 		BecquerelMonitor.IconPanel iconPanel1;
 		System.Windows.Forms.Label label6;
-	}
+        private Label label7;
+        private Label label8;
+        private Label label9;
+    }
 }
