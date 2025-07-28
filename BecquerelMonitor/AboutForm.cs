3fbb6973c07656fc44540fd950d108ac7cba90aa
Amber <morozov.albert@yandex.ru>
2025-07-28 15:08:01 +0300

    AboutForm. Add clicks to open URL/s in browser.

--- BecquerelMonitor/AboutForm.cs
+++ BecquerelMonitor/AboutForm.cs
@@ -1,5 +1,6 @@
 ﻿using BecquerelMonitor.Properties;
 using System;
+using System.Diagnostics;
 using System.Windows.Forms;
 
 namespace BecquerelMonitor
@@ -17,6 +18,36 @@ namespace BecquerelMonitor
         void button1_Click(object sender, EventArgs e)
         {
             base.Close();
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string url = "https://github.com/Am6er/BecqMoni";
+                Process.Start(new ProcessStartInfo(url) { UseShellExecute = true });
+            }
+            catch (Exception) { }
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string url = "https://www.youtube.com/@Am6er";
+                Process.Start(new ProcessStartInfo(url) { UseShellExecute = true });
+            }
+            catch (Exception) { }
+        }
+
+        private void label9_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string url = "https://rutube.ru/channel/30585350/";
+                Process.Start(new ProcessStartInfo(url) { UseShellExecute = true });
+            }
+            catch (Exception) { }
         }
     }
 }
