164e513ad0589458b4a2549c4606adb1ce5e470f
Amber <morozov.albert@yandex.ru>
2026-06-16 23:12:46 +0300

    Fix form editing in Designer

---
+++ BecquerelMonitor/TestForm.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace BecquerelMonitor
+{
+    partial class TestForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            XPTable.Models.DataSourceColumnBinder dataSourceColumnBinder1 = new XPTable.Models.DataSourceColumnBinder();
+            XPTable.Renderers.DragDropRenderer dragDropRenderer1 = new XPTable.Renderers.DragDropRenderer();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TestForm));
+            this.table1 = new XPTable.Models.Table();
+            this.columnModel1 = new XPTable.Models.ColumnModel();
+            this.textColumn1 = new XPTable.Models.TextColumn();
+            this.numberColumn1 = new XPTable.Models.NumberColumn();
+            this.doubleColumn1 = new XPTable.Models.DoubleColumn();
+            this.tableModel1 = new XPTable.Models.TableModel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.table1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // table1
+            // 
+            this.table1.BorderColor = System.Drawing.Color.Black;
+            this.table1.ColumnModel = this.columnModel1;
+            this.table1.DataMember = null;
+            this.table1.DataSourceColumnBinder = dataSourceColumnBinder1;
+            dragDropRenderer1.ForeColor = System.Drawing.Color.Red;
+            this.table1.DragDropRenderer = dragDropRenderer1;
+            this.table1.GridLinesContrainedToData = false;
+            resources.ApplyResources(this.table1, "table1");
+            this.table1.Name = "table1";
+            this.table1.TableModel = this.tableModel1;
+            this.table1.UnfocusedBorderColor = System.Drawing.Color.Black;
+            // 
+            // columnModel1
+            // 
+            this.columnModel1.Columns.AddRange(new XPTable.Models.Column[] {
+            this.textColumn1,
+            this.numberColumn1,
+            this.doubleColumn1});
+            // 
+            // textColumn1
+            // 
+            this.textColumn1.IsTextTrimmed = false;
+            resources.ApplyResources(this.textColumn1, "textColumn1");
+            // 
+            // numberColumn1
+            // 
+            this.numberColumn1.IsTextTrimmed = false;
+            resources.ApplyResources(this.numberColumn1, "numberColumn1");
+            // 
+            // doubleColumn1
+            // 
+            this.doubleColumn1.IsTextTrimmed = false;
+            resources.ApplyResources(this.doubleColumn1, "doubleColumn1");
+            // 
+            // button1
+            // 
+            resources.ApplyResources(this.button1, "button1");
+            this.button1.Name = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // panel1
+            // 
+            resources.ApplyResources(this.panel1, "panel1");
+            this.panel1.Name = "panel1";
+            // 
+            // TestForm
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.table1);
+            this.Name = "TestForm";
+            ((System.ComponentModel.ISupportInitialize)(this.table1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private XPTable.Models.Table table1;
+        private XPTable.Models.ColumnModel columnModel1;
+        private XPTable.Models.TextColumn textColumn1;
+        private XPTable.Models.NumberColumn numberColumn1;
+        private XPTable.Models.DoubleColumn doubleColumn1;
+        private XPTable.Models.TableModel tableModel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
