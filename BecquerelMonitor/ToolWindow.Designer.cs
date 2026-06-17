b9eacacf0a2c06e52faa6369d6ec824e16c20166
Amber <morozov.albert@yandex.ru>
2026-06-17 14:46:47 +0300

    Fix ToolWindow design refresh

--- BecquerelMonitor/ToolWindow.Designer.cs
+++ BecquerelMonitor/ToolWindow.Designer.cs
@@ -16,40 +16,51 @@
 		// Token: 0x060000DB RID: 219 RVA: 0x00004BE8 File Offset: 0x00002DE8
 		void InitializeComponent()
 		{
-			this.components = new global::System.ComponentModel.Container();
-			this.contextMenuStrip1 = new global::System.Windows.Forms.ContextMenuStrip(this.components);
-			this.option1ToolStripMenuItem = new global::System.Windows.Forms.ToolStripMenuItem();
-			this.option2ToolStripMenuItem = new global::System.Windows.Forms.ToolStripMenuItem();
-			this.option3ToolStripMenuItem = new global::System.Windows.Forms.ToolStripMenuItem();
-			this.contextMenuStrip1.SuspendLayout();
-			base.SuspendLayout();
-			this.contextMenuStrip1.Items.AddRange(new global::System.Windows.Forms.ToolStripItem[]
-			{
-				this.option1ToolStripMenuItem,
-				this.option2ToolStripMenuItem,
-				this.option3ToolStripMenuItem
-			});
-			this.contextMenuStrip1.Name = "contextMenuStrip1";
-			this.contextMenuStrip1.Size = new global::System.Drawing.Size(122, 70);
-			this.option1ToolStripMenuItem.Name = "option1ToolStripMenuItem";
-			this.option1ToolStripMenuItem.Size = new global::System.Drawing.Size(121, 22);
-			this.option1ToolStripMenuItem.Text = "Option&1";
-			this.option2ToolStripMenuItem.Name = "option2ToolStripMenuItem";
-			this.option2ToolStripMenuItem.Size = new global::System.Drawing.Size(121, 22);
-			this.option2ToolStripMenuItem.Text = "Option&2";
-			this.option3ToolStripMenuItem.Name = "option3ToolStripMenuItem";
-			this.option3ToolStripMenuItem.Size = new global::System.Drawing.Size(121, 22);
-			this.option3ToolStripMenuItem.Text = "Option&3";
-			base.AutoScaleDimensions = new global::System.Drawing.SizeF(6f, 12f);
-			base.AutoScaleMode = global::System.Windows.Forms.AutoScaleMode.Font;
-			base.ClientSize = new global::System.Drawing.Size(292, 246);
-			base.DockAreas = (global::WeifenLuo.WinFormsUI.Docking.DockAreas.Float | global::WeifenLuo.WinFormsUI.Docking.DockAreas.DockLeft | global::WeifenLuo.WinFormsUI.Docking.DockAreas.DockRight | global::WeifenLuo.WinFormsUI.Docking.DockAreas.DockTop | global::WeifenLuo.WinFormsUI.Docking.DockAreas.DockBottom);
-			this.Font = new global::System.Drawing.Font("MS UI Gothic", 9f, global::System.Drawing.FontStyle.Regular, global::System.Drawing.GraphicsUnit.Point, 128);
-			base.Name = "ToolWindow";
-			base.TabText = "ToolWindow";
-			this.Text = "ToolWindow";
-			this.contextMenuStrip1.ResumeLayout(false);
-			base.ResumeLayout(false);
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ToolWindow));
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.option1ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.option2ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.option3ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.option1ToolStripMenuItem,
+            this.option2ToolStripMenuItem,
+            this.option3ToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            resources.ApplyResources(this.contextMenuStrip1, "contextMenuStrip1");
+            // 
+            // option1ToolStripMenuItem
+            // 
+            this.option1ToolStripMenuItem.Name = "option1ToolStripMenuItem";
+            resources.ApplyResources(this.option1ToolStripMenuItem, "option1ToolStripMenuItem");
+            // 
+            // option2ToolStripMenuItem
+            // 
+            this.option2ToolStripMenuItem.Name = "option2ToolStripMenuItem";
+            resources.ApplyResources(this.option2ToolStripMenuItem, "option2ToolStripMenuItem");
+            // 
+            // option3ToolStripMenuItem
+            // 
+            this.option3ToolStripMenuItem.Name = "option3ToolStripMenuItem";
+            resources.ApplyResources(this.option3ToolStripMenuItem, "option3ToolStripMenuItem");
+            // 
+            // ToolWindow
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.DockAreas = ((WeifenLuo.WinFormsUI.Docking.DockAreas)(((((WeifenLuo.WinFormsUI.Docking.DockAreas.Float | WeifenLuo.WinFormsUI.Docking.DockAreas.DockLeft) 
+            | WeifenLuo.WinFormsUI.Docking.DockAreas.DockRight) 
+            | WeifenLuo.WinFormsUI.Docking.DockAreas.DockTop) 
+            | WeifenLuo.WinFormsUI.Docking.DockAreas.DockBottom)));
+            this.Name = "ToolWindow";
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
 		}
 
 		// Token: 0x04000054 RID: 84
