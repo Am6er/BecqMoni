07ce93a775c4eac24044ed4edf44687333f3c6f2
Amber <morozov.albert@yandex.ru>
2026-07-02 15:21:58 +0300

    RJMCMC performace fixes

--- BecquerelMonitor/RjmcmcDeconvolution/RjmcmcResult.cs
+++ BecquerelMonitor/RjmcmcDeconvolution/RjmcmcResult.cs
@@ -5,12 +5,10 @@ namespace BecquerelMonitor.RjmcmcDeconvolution
     internal sealed class RjmcmcResult
     {
         public List<RjmcmcPeakCandidate> ExtraCandidates { get; private set; }
-        public List<RjmcmcRoi> ProcessedRois { get; private set; }
 
         public RjmcmcResult()
         {
             ExtraCandidates = new List<RjmcmcPeakCandidate>();
-            ProcessedRois = new List<RjmcmcRoi>();
         }
     }
 }
